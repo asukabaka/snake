8278d55c2ab708929bce8e1dd17ae0d93fdcbd7b
asukabaka <48495833+asukabaka@users.noreply.github.com>
2020-04-15 18:41:07 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,42 +13,56 @@ function setup() {
 }
 
 function pickLocation() {
-  var cols = floor(width/scl);
-  var rows = floor(height/scl);
+  var cols = floor(width / scl);
+  var rows = floor(height / scl);
   food = createVector(floor(random(cols)), floor(random(rows)));
   food.mult(scl);
 }
 
 function draw() {
 
-switch (state) {
+  switch (state) {
 
-  case ('splash'):
-    start();
-  break;
+    case ('splash'):
+      start();
+      break;
 
-  case ('gamePlay'):
-    gamePlay();
-  break;
+    case ('gamePlay'):
+      gamePlay();
+      break;
+  }
+
 }
 
-}
+function mousePressed() {
+  switch (state) {
 
-function start(){
-  background('51');
-  text('click to adopt doggos', 500, 500);
-  function mousePressed() {
-    state = 'gamePlay';
+    case ('splash'):
+      startMousePressed();
+      break;
+
+    case ('gamePlay'):
+      gamePlayMousePressed();
+      break;
   }
 }
 
-function gamePlay(){
+function startMousePressed() {
+  state = gamePlay;
+}
+
+function start() {
+  background('51');
+  text('click to adopt doggos', 500, 500);
+}
+
+function gamePlay() {
   background('rgba(45,225,45, 0.25)');
   s.update();
   s.show();
   s.death();
 
-if (s.eat(food)) {
+  if (s.eat(food)) {
     pickLocation();
   }
 
@@ -58,12 +72,12 @@ if (s.eat(food)) {
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    s.dir(0,-1);
+    s.dir(0, -1);
   } else if (keyCode === DOWN_ARROW) {
-    s.dir(0,1);
+    s.dir(0, 1);
   } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1,0);
+    s.dir(1, 0);
   } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1,0);
+    s.dir(-1, 0);
   }
 }
